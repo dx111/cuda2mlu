2497dc9108e0f5b6084983453915334486ec0793
段翔 <dx2077@outlook.com>
2023-08-19 07:20:19 +0000

    update

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ import os
 
 setup(
     name="cuda2mlu",
-    version="0.1",
-    description="A package that rewrites torch device CUDA  to use MLU",
+    version="0.0.1",
+    description="rewrites torch CUDA device to use MLU",
+    long_description = "A package that rewrites torch device CUDA to use MLU",
     packages=["cuda2mlu"],
     install_requires=["torch"],
     setup_requires=["torch"],
